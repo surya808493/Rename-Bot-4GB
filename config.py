6b2496fe68f20e8b15d1bc918abf3ab98832ea5c
surya808493 <surajkumar75419696@gmail.com>
2024-12-21 13:08:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,23 +3,23 @@ import os
 
 
 # Required Variables Config
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
+API_ID = int(os.environ.get("API_ID", "26992030"))
+API_HASH = os.environ.get("API_HASH", "4da7d71c6bc4512a886e41aca83a5ee3")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8026778102:AAEsTeZXg7kAstTC2jtmqSchmLRcZD-Vyds")
+ADMIN = int(os.environ.get("ADMIN", "6476706511"))
 
 
 # Premium 4GB Renaming Client Config
-STRING_SESSION = os.environ.get("STRING_SESSION", "")
+STRING_SESSION = os.environ.get("STRING_SESSION", "BQGb3Z4AeG-uMZw7uhir-O-ilHofVN0a3bCvtKwmAumO7M3it7BTQGWxitqNtQBsO0VLINgZ-BD1q7pPc9T_Va2-7UNZhQcsMiofB3dJfEpWsxC1NSDDTab7UYQYoErGS6p65b_nUo5gKjB-vnpR_qSPNG556akoVrKqCRmFK64epD707ANAu6hBEFhqC35e8CL-s2zPe6zHR2y4-JrL9D03CygVCtT4rzGOdn9zyn5GYp1V_x564S1spOABmry3FnIFkwKoA7ca9JTkWiJBUTwtlpuXwyTUiEbH_iTEdJKGBxolQPe3sWrHa2MFjaHPqSIMiywNdywinGVbYU0gXzEBXcKC_QAAAAGCCrLPAA")
 
 
 # Log & Force Channel Config
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "-1002050862981")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002280223922"))
 
 
 # Mongo DB Database Config
-DATABASE_URL = os.environ.get("DATABASE_URL", "")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://srjcomedyrecord:CSL2FYJErFf6jAX8@cluster0.frtra.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "madflixbotz")
 
 
